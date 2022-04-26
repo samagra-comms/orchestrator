ae810936f4cb738f7bbb15b679fde19a8a8e182c
Yashwant <yashwant.tejwani@aurigait.com>
2022-04-26 12:45:51 +0530

    minor changes

---
+++ src/test/java/com/uci/orchestrator/Consumer/ReactiveConsumerTest.java
@@ -0,0 +1,12 @@
+package com.uci.orchestrator.Consumer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReactiveConsumerTest {
+
+    @Test
+    void onMessage() {
+    }
+}
---
+++ src/test/java/com/uci/orchestrator/Controllers/MessageRosaTest.java
@@ -0,0 +1,16 @@
+package com.uci.orchestrator.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageRosaTest {
+
+    @Test
+    void greeting() {
+    }
+
+    @Test
+    void deleteLastMessage() {
+    }
+}
